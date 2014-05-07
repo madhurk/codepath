5c2587fcc4277b103d60d7791c9b6ec1847ebbcd
Madhur Khandelwal <madhurk@gmail.com>
2014-05-07 15:23:56 -0700

    playing with settings menu

--- SimpleToDo/src/codepath/apps/simpletodo/TodoActivity.java
+++ SimpleToDo/src/codepath/apps/simpletodo/TodoActivity.java
@@ -9,6 +9,7 @@ import org.apache.commons.io.FileUtils;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
@@ -110,5 +111,12 @@ public class TodoActivity extends Activity {
     }catch(IOException e){
       e.printStackTrace();
     }
+  }
+  
+  @Override
+  public boolean onCreateOptionsMenu(Menu menu) {
+    // Inflate the menu; this adds items to the action bar if it is present.
+    getMenuInflater().inflate(R.menu.todo, menu);
+    return true;
   }
 }
