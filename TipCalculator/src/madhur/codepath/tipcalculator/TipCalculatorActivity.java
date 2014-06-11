888bf38aeba1799ea20c019084df278e3e15b1b0
Madhur Khandelwal <madhurk@gmail.com>
2014-06-11 16:02:20 -0700

    refactor file names. change app icon

--- TipCalculator/src/madhur/codepath/tipcalculator/TipCalculatorActivity.java
+++ TipCalculator/src/madhur/codepath/tipcalculator/TipCalculatorActivity.java
@@ -13,14 +13,14 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
-public class MainActivity extends Activity {
+public class TipCalculatorActivity extends Activity {
 
   final int step = 5;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_main);
+    setContentView(R.layout.activity_tip_calculator);
 
     TipListener tipListener = new TipListener();
 
