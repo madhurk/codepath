6ee0d112b87f7bd5b6707fab3ae3470bc0551f39
Madhur Khandelwal <madhurk@gmail.com>
2014-06-17 00:01:30 -0700

    add share functionality on full image screen

--- ImageSearcher/src/madhur/codepath/imagesearcher/ImageDisplayActivity.java
+++ ImageSearcher/src/madhur/codepath/imagesearcher/ImageDisplayActivity.java
@@ -1,12 +1,18 @@
 package madhur.codepath.imagesearcher;
 
-import com.loopj.android.image.SmartImageView;
-
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ShareActionProvider;
+
+import com.loopj.android.image.SmartImageView;
 
 public class ImageDisplayActivity extends Activity {
+
+  private ShareActionProvider mShareActionProvider;
+  private String fullUrl;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
@@ -18,5 +24,34 @@ public class ImageDisplayActivity extends Activity {
     
     SmartImageView iv = (SmartImageView)findViewById(R.id.ivResult);
     iv.setImageUrl(image.fullUrl);
+    fullUrl = image.fullUrl;
+  }
+  
+  @Override
+  public boolean onCreateOptionsMenu(Menu menu) {
+      // Inflate menu resource file.
+      getMenuInflater().inflate(R.menu.display_menu, menu);
+
+      // Locate MenuItem with ShareActionProvider
+      MenuItem item = menu.findItem(R.id.action_share);
+
+      // Fetch and store ShareActionProvider
+      mShareActionProvider = (ShareActionProvider) item.getActionProvider();
+      
+      Intent myIntent = new Intent();
+      myIntent.setAction(Intent.ACTION_SEND);
+      myIntent.putExtra(Intent.EXTRA_TEXT, fullUrl );
+      myIntent.setType("text/plain");
+
+      mShareActionProvider.setShareIntent(myIntent);
+      // Return true to display menu
+      return true;
+  }
+
+  // Call to update the share intent
+  private void setShareIntent(Intent shareIntent) {
+      if (mShareActionProvider != null) {
+          mShareActionProvider.setShareIntent(shareIntent);
+      }
   }
 }
